a7c121ede56122d6c19d02eb62df63760de56eea
Vikalp2003 <78001726+Vikalp2003@users.noreply.github.com>
2023-04-20 23:58:17 +0530

    Add files via upload

---
+++ lib/model/todo.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
+      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
+      ToDo(id: '03', todoText: 'Check Emails', isDone: true),
+      ToDo(id: '04', todoText: 'Team Meeting'),
+      ToDo(id: '05', todoText: 'Work on mobile apps'),
+      ToDo(id: '06', todoText: 'Dinner with Family'),
+      
+    ];
+  }
+}
